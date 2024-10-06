a011078ea078c69cca633dc812b1fe8c071190d7
Matheus Felipe <felipe_2012matheus@hotmail.com>
2024-10-06 01:27:44 -0300

    Estruturação do projeto e criação do header

---
+++ projeto-alianca-saude/src/components/Header/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Header.css';
+import imgHeader from '../../assets/logo.png';
+
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="logo">
+                <a href="/">
+                    <img className="logo" src={imgHeader} alt="Logo Missão com Saúde"/>
+                </a>
+            </div>
+            <nav>
+                <ul className="nav-links">
+                    <li><a href='/'>Check UP</a></li>
+                    <li>
+                        <a href='/'>Sobre</a>
+                        <ul className="nav-links-dropdown">
+                            <li><a href="/">Projeto</a></li>
+                            <li><a href="/">Equipe</a></li>
+                        </ul>
+                    </li>
+                    <li><a href='/'>Notícias</a></li>
+                    <li><a href='/'>SOS Ajuda</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
---
+++ projeto-alianca-saude/src/pages/home/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+
+const Home = () => (
+    <Header/>
+    
+);
+
+export default Home;
